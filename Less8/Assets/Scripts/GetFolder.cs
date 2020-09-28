9c8bc3348c9e971d58b32192723f99eacd33b4b3
Aleksey <burovalexprogrammer@gmail.com>
2020-09-28 12:56:41 +0500

    Фикс хэша
    f43326edd398a372229f105255f03960

--- Less8/Assets/Scripts/GetFolder.cs
+++ Less8/Assets/Scripts/GetFolder.cs
@@ -13,29 +13,21 @@ public class GetFolder : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        var s1 = "ab";
-
-        using (var md5 = MD5.Create())
-        {
-            var checksum = md5.ComputeHash(Encoding.ASCII.GetBytes(s1));
-            var output = BitConverter.ToString(checksum).Replace("-","").ToLower();
-        }
-
         var x = Application.dataPath;
         var sceneFiles = Directory.GetFiles(x, "*.unity", SearchOption.AllDirectories);
-        var byteList = new List<byte[]>();
+        var stringList = new List<string>();
         foreach (var sceneFile in sceneFiles)
         {
             if (!File.Exists(sceneFile)) Debug.LogError("Файл не найден, путь: "+sceneFile);
             var sceneFileInfo = new FileInfo(sceneFile);
             if (sceneFileInfo.Extension != ".unity") continue;
-            byteList.Add(File.ReadAllBytes(sceneFile));
+            stringList.Add(File.ReadAllText(sceneFile));
         }
 
-        var byteSum = byteList.Aggregate((b1, b2) => b1.Concat(b2).ToArray());
+        var stringSum = stringList.Aggregate((s1, s2) => s1 + s2);
         using (var md5 = MD5.Create())
         {
-            var hash = md5.ComputeHash(byteSum);
+            var hash = md5.ComputeHash(Encoding.ASCII.GetBytes(stringSum));
             var hashSum = BitConverter.ToString(hash).Replace("-", "").ToLower();
         }
     }
