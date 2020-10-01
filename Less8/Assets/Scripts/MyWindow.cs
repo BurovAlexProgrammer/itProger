bd1d077dc5797d34edc2ffbf4768d17af54e46a7
Aleksey <burovalexprogrammer@gmail.com>
2020-10-01 07:16:28 +0500

    Создание проекта Lesson9

---
+++ Less8/Assets/Scripts/MyWindow.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class MyWindow : EditorWindow
+{
+    public string gg;
+
+    [MenuItem("Tools/MyChecksum")]
+    public static void SetChecksum()
+    {
+        EditorWindow.GetWindow<MyWindow>();
+
+    }
+
+    public void OnGUI()
+    {
+        EditorGUILayout.BeginHorizontal();
+        EditorGUILayout.LabelField("Project checksum");
+        var str = EditorGUILayout.TextField("");
+        EditorGUILayout.EndHorizontal();
+    }
+
+    public void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(Vector3.zero, 0.25f);
+    }
+}
