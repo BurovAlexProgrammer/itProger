dc1f52bd14cf28222839eaa17b7866b9af2af194
Aleksey <burovalexprogrammer@gmail.com>
2020-10-01 22:43:03 +0500

    Добавил скрипты и анимацию
    Скрипты Follow Camera & LookAtTarget

---
+++ Less9_Animation/Assets/FollowTarget.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowTarget : MonoBehaviour
+{
+    [SerializeField]
+    Transform followTarget;
+    [SerializeField]
+    Vector3 followOffset;
+    [SerializeField]
+    Transform lookAtTarget;
+    [SerializeField]
+    Vector3 lookAtOffset;
+    [SerializeField]
+    float followSpeed;
+
+    Vector3 velocity = Vector3.zero;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (followTarget != null)
+        {
+            var desiredPosition = followTarget.position + followOffset;
+            transform.position = Vector3.SmoothDamp(transform.position, desiredPosition, ref velocity, followSpeed);
+        }
+
+        if (lookAtTarget != null)
+            transform.LookAt(lookAtTarget.position + lookAtOffset);
+
+    }
+}
---
+++ Less9_Animation/Assets/PlayerControl.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    [SerializeField]
+    float moveSpeed;
+    [SerializeField]
+    float rotateSpeed;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        var hAxis = Input.GetAxis("Horizontal");
+        var vAxis = Input.GetAxis("Vertical");
+        if (hAxis != 0)
+            transform.Rotate(Vector3.up, rotateSpeed * Time.deltaTime * hAxis);
+        if (vAxis != 0)
+            transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime * vAxis);
+    }
+}
