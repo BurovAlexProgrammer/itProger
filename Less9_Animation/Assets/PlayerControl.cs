5bf69851341606a40e7235e32f603fb5173463b9
Aleksey <burovalexprogrammer@gmail.com>
2020-10-02 11:04:20 +0500

    Урок9 финал
    Настроил аниматор, управление игроком, физику. Всё

--- Less9_Animation/Assets/PlayerControl.cs
+++ Less9_Animation/Assets/PlayerControl.cs
@@ -2,25 +2,39 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Animator))]
 public class PlayerControl : MonoBehaviour
 {
     [SerializeField]
     float moveSpeed;
     [SerializeField]
-    float rotateSpeed;
+    float rotateSpeedX;
+    [SerializeField]
+    float rotateSpeedY;
+    [SerializeField]
+    Transform cameraPivot;
+
+    Animator animator;
 
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
     }
 
     void Update()
     {
         var hAxis = Input.GetAxis("Horizontal");
         var vAxis = Input.GetAxis("Vertical");
+        var vMouse = Input.GetAxis("Mouse Y");
+
         if (hAxis != 0)
-            transform.Rotate(Vector3.up, rotateSpeed * Time.deltaTime * hAxis);
+            transform.Rotate(Vector3.up, rotateSpeedX * Time.deltaTime * hAxis);
         if (vAxis != 0)
             transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime * vAxis);
+        if (vMouse != 0)
+            cameraPivot.Rotate(Vector3.right, rotateSpeedY * Time.deltaTime * vMouse);
+
+        animator.SetFloat("moveSpeed", vAxis);
+        animator.SetBool("isRun", vAxis != 0);
     }
 }
