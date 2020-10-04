3377bfd75f7b715e8d404051388842b5241bf987
Aleksey <burovalexprogrammer@gmail.com>
2020-10-04 22:02:34 +0500

    Поворот машин на перекрестке
    Настроил триггеры поворота и написал соответствующий скрипт

---
+++ Less11/Assets/Scripts/CarController.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class CarController : MonoBehaviour
+{
+    [SerializeField]
+    float speed = 15f,
+        turnRightMult = 6f,
+        turnLeftMult = -3f;
+
+    [SerializeField]
+    bool turnRight, turnLeft;
+
+    Rigidbody rigidbody = null;
+
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        rigidbody.MovePosition(transform.position - transform.forward * speed * Time.fixedDeltaTime);
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.transform.CompareTag("TurnRightBox") && turnRight)
+        {
+            float rotateSpeed = speed * turnRightMult;
+            Quaternion deltaRotation = Quaternion.Euler(new Vector3(0, rotateSpeed, 0) * Time.fixedDeltaTime);
+            rigidbody.MoveRotation(rigidbody.rotation * deltaRotation);
+        }
+        if (other.transform.CompareTag("TurnLeftBox") && turnLeft)
+        {
+            float rotateSpeed = speed * turnLeftMult;
+            Quaternion deltaRotation = Quaternion.Euler(new Vector3(0, rotateSpeed, 0) * Time.fixedDeltaTime);
+            rigidbody.MoveRotation(rigidbody.rotation * deltaRotation);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.transform.CompareTag("TurnRightBox") && turnRight || 
+            other.transform.CompareTag("TurnLeftBox") && turnLeft)
+        {
+            var rotY = rigidbody.rotation.eulerAngles.y;
+            var newRotY = Mathf.Round(rotY / 90f) * 90;
+            rigidbody.rotation = Quaternion.Euler(0, newRotY, 0);
+        }
+
+
+    }
+}
---
+++ Less11/Assets/Scripts/HideOnPlay.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HideOnPlay : MonoBehaviour
+{
+    private void Awake()
+    {
+        var meshRenderer = GetComponent<MeshRenderer>();
+        if (meshRenderer != null)
+            meshRenderer.enabled = false;
+    }
+}
